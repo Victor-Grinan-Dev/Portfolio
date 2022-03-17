d2de22841bb57e6f88736a7b6a234b940b97a64f
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-03-17 21:00:42 +0200

    currencies added full of bugs

--- projects/08-currencies/index.js
+++ projects/08-currencies/index.js
@@ -1,4 +1,34 @@
-import { FetchWrapper } from "./fetcwrapper";
+class FetchWrapper {
+  constructor(baseURL) {
+    this.baseURL = baseURL;
+  }
+
+  get(endpoint) {
+    return fetch(this.baseURL + endpoint).then((response) => response.json());
+  }
+
+  put(endpoint, body) {
+    return this._send("put", endpoint, body);
+  }
+
+  post(endpoint, body) {
+    return this._send("post", endpoint, body);
+  }
+
+  delete(endpoint, body) {
+    return this._send("delete", endpoint, body);
+  }
+
+  _send(method, endpoint, body) {
+    return fetch(this.baseURL + endpoint, {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    }).then((response) => response.json());
+  }
+}
 const baseURL = "https://v6.exchangerate-api.com/v6/66507ff79c1dc7c61ec22725/latest"
 
 const conversionResult = document.querySelector("#conversion-result");
