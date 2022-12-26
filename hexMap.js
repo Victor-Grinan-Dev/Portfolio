1be90be60619ccc007db46b65e80a80796cdce45
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-12-26 11:30:49 +0200

    updated

--- hexMap.js
+++ hexMap.js
@@ -1,9 +1,16 @@
 const hexArea = document.querySelector(".hexMap");
 
+let width = document.body.clientWidth;
+let height = document.body.clientHeight;
+
+let rows = 
+
 window.addEventListener("resize", function(event) {
-    const width = document.body.clientWidth;
-    const height = document.body.clientHeight;
+    width = document.body.clientWidth;
+    height = document.body.clientHeight;
 
     console.log(width);
     console.log(height);
-})
+});
+
+hexArea.innerHTML = '<div class="tile"></div>';
