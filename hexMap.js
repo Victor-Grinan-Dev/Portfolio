de92067b03f4c9f6f55cc74ca820dea08e537bda
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-12-29 10:35:38 +0200

    added files and updated css

--- hexMap.js
+++ hexMap.js
@@ -1,10 +1,16 @@
-const hexArea = document.querySelector(".hexMap");
+import blank from './hexes/blank.png';
+import blue from './hexes/selected.png';
+import red from './hexes/hostile.png';
 
+alert('hello')
+
+const hexArea = document.querySelector(".hexMap");
 
 window.addEventListener("resize", function(event) {
     const width = document.body.clientWidth;
     const height = document.body.clientHeight;
-
     console.log(width);
     console.log(height);
-})
+});
+
+hexArea.innerHTML = '<div style.backgrounfImage={url(`${blank}`)}>hex</div>'
