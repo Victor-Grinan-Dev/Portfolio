5e74da2b28b8327cfcca8d722f9ecb0fb14e1c6a
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-12-29 17:06:01 +0200

    deleted hexmap.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const grinan = document.querySelector("#grinan");
 const g = document.querySelector("#g")
 const victor = document.querySelector("#victor");
@@ -115,3 +114,37 @@ function topFunction() {
 }
 
 scrollUp.addEventListener("click", topFunction);
+
+/* hex Map */
+
+
+const hexMap = document.querySelector(".hexMap");
+const hexSize = 50;
+
+let width = document.body.clientWidth;
+let height = document.body.clientHeight;
+let rows = (height/ 4) / hexSize;
+let columns = (width / hexSize) - 1;
+
+window.addEventListener("resize", function(event) {
+    width = document.body.clientWidth;
+    height = document.body.clientHeight;
+
+    rows = (height/ 10) / hexSize ;
+    columns = (width / hexSize) - 2;
+});
+
+
+const createHexMatrix = () => {
+    for(let i = 0; i < rows; i++){
+        for (let j = 0; j < columns; j++){
+            const blank = document.createElement('img');
+            blank.id=`${i}${j}`;
+            blank.src = i % 2 === 0 ?'./hexes/selected.png': './hexes/hostile.png';
+            blank.classList = 'hex';
+            blank.style.left = i % 2 === 0 ? `${j * hexSize}px`: `${j * hexSize + hexSize/2}px`;
+            blank.style.top = `${i * (hexSize * 0.75)}px`;
+            hexMap.appendChild(blank);
+        }
+    }
+}
