12c5ca3e559952df8510724699b806a62da48e90
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-04-04 11:10:02 +0300

    add link to food app

--- main.js
+++ main.js
@@ -195,7 +195,7 @@ const projectsData=[
         "id":"foodApp",
         "title":"Food App",
         "type":"secundary",
-        "link":"https://github.com/Victor-Grinan-Dev/food-app3",
+        "link":"food-app4.netlify.app",
         //https://victor-grinan-dev.github.io/food-app3/
         "imgUrl":"./images/test_Img.jpg",
         "description":"Pagination & Firebase CRUD",
