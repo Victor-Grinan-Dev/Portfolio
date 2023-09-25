809d4e0309657f213d95c473d2d4058d1e0c5e89
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-09-25 23:08:00 +0300

    add carousell logics

--- main.js
+++ main.js
@@ -1522,6 +1522,98 @@ const showAllCertificates = () => {
 };
 moreCertificatesBtn.addEventListener("click", showAllCertificates);
 
+/*certificates carousell */
+
+const track = document.querySelector('.carousel__track');
+const nextButton = document.querySelector('.carousel__button--right');
+const prevButton = document.querySelector('.carousel__button--left');
+const dotsNav = document.querySelector('.carousel__nav');
+
+allCertificate
+
+allCertificate.forEach((cert, i) => {
+    track.innerHTML += `
+        <li id="slide${i}" name="slide${i}" class="carousel__slide slide${i} ${i === 0 ? "current_slide" : ""}">
+            <img src="${cert.docLink}" alt="slide${cert.id}" class="carousel__image"/>
+        </li>
+    `;
+    dotsNav.innerHTML += `
+        <div class="carousel__indicator ${i === 0? "current_dot" : ""} "></div
+    `
+});
+
+const slides = Array.from(track.children);
+const dots = Array.from(dotsNav.children);
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+const setSlidePosition = (slide, i) => {
+    slide.style.left = `${i * slideWidth}px`;
+}
+
+slides.forEach(setSlidePosition);
+
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('current_dot');
+    targetDot.classList.add('current_dot');
+}
+
+const hideShowArrows = (targetIndex) => {
+    if (targetIndex === 0){
+        prevButton.classList.add('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    }else if(targetIndex === slides.length - 1){
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.add('is-hidden');
+    }else{
+        nextButton.classList.remove('is-hidden');
+        prevButton.classList.remove('is-hidden');
+    }
+}
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('current_slide');
+    targetSlide.classList.add('current_slide');
+}
+
+nextButton.addEventListener('click', () => {
+    const currentSlide = track.querySelector('.current_slide');
+    // console.log(track)
+    const nextSlide = currentSlide.nextElementSibling;
+    const targetIndex = slides.findIndex(slide => slide === nextSlide);
+    const currentDot = dotsNav.querySelector('.current_dot');
+    const targetDot = currentDot.nextElementSibling;
+    moveToSlide(track, currentSlide, nextSlide);
+    hideShowArrows(targetIndex);
+    updateDots(currentDot, targetDot);
+});
+
+prevButton.addEventListener('click', () => {
+    const currentSlide = track.querySelector('.current_slide');
+    const prevSlide = currentSlide.previousElementSibling;
+    const targetIndex = slides.findIndex(slide => slide === prevSlide);
+    const currentDot = dotsNav.querySelector('.current_dot');
+    const targetDot = currentDot.previousElementSibling;
+    moveToSlide(track, currentSlide, prevSlide);
+    hideShowArrows(targetIndex);
+    updateDots(currentDot, targetDot);
+});
+
+dotsNav.addEventListener('click', e =>{
+    const targetDot = e.target.closest('button');
+    if(!targetDot)return;
+   
+    const currentSlide = track.querySelector('.current_slide');
+    const currentDot = dotsNav.querySelector('.current_dot');
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+    hideShowArrows(targetIndex);
+});
+
 /* copyrights */
 const year = new Date();
 const yearSpan = document.querySelector(".year");
@@ -1565,3 +1657,5 @@ const createHexMatrix = () => {
     }
   }
 };
+
+
